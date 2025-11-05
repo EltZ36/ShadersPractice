547fb9c68a4d1b83c5841d733181842a1a42d93d
Elton Zeng <poptako9@gmail.com>
2025-11-04 18:06:02 -0800

    First init and adding in the first files for the project.

---
+++ index.js
@@ -0,0 +1,11 @@
+var simpleShader = loadShader("color.vert", "color.frag");
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  ellipse(200, 200, 50, 50);
+  shader(simpleShader);
+}
